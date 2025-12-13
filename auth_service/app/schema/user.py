a60252fd64aab5825ecce06cac1799615fdea192
Aman Raj <billygates124356@gmail.com>
2025-12-13 13:00:32 +0530

    Added user schemas

---
+++ auth_service/app/schema/user.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field, EmailStr
+
+from typing import Literal
+from datetime import datetime
+from bson import ObjectId
+
+from schema.object_id import PyObjectId
+
+
+# ------------- BASE CLASS FOR USER --------------
+class UserBase(BaseModel):
+    email : EmailStr
+    name  : str
+
+    role  : Literal["customer", "staff", "admin"] = "customer"
+
+
+# ------------- CLASSES FOR HTTP HANDLERS --------
+class UserCreate(UserBase):
+    password : str = Field(min_length = 8, max_length = 64)
+
+class UserLogin(BaseModel):
+    email : EmailStr
+    password : str
+
+class UserResponse(UserBase):
+
+    id : PyObjectId = Field(alias = "_id", default_factory = PyObjectId)
+
+    created_at : datetime = Field(default_factory = datetime.now)
+    updated_at : datetime = Field(default_factory = datetime.now) 
+
+    class Config:
+        json_encoders = {
+            ObjectId : str 
+        }
+        allow_population_by_field_name = True 
+        arbitrary_types_allowed = True
+
+
+# --------------- CLASSES FOR MONGODB -------------
+class UserInDB(UserResponse):
+    hashed_password : str 
