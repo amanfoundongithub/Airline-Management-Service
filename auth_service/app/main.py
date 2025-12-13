f257cd868b764d09278c43f6d1e116808a8a0052
Aman Raj <billygates124356@gmail.com>
2025-12-13 15:05:24 +0530

    Integrated router in main

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from db.client import connect_with_mongo, disconnect_with_mongo
 
+import api.v1.user_router as user_router
 
 # App
-app = FastAPI()
+app = FastAPI(title = "Airline Auth Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,            
+    allow_methods=["*"],                
+    allow_headers=["*"],
+)
 
 # Hooks
 app.add_event_handler("startup", connect_with_mongo)
 app.add_event_handler("shutdown", disconnect_with_mongo)
+
+# Routers
+app.include_router(user_router.router, prefix = "/api/v1")
+
+
+@app.get("/")
+def read_root():
+    return {
+        "status" : "Auth Service running",
+        "version" : "v1"
+    }
 
