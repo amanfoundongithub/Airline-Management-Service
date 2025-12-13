e27f0bef6d8aab67582d738517da65b8fef2a969
Aman Raj <billygates124356@gmail.com>
2025-12-13 15:00:20 +0530

    Added method to review token

--- auth_service/app/core/security.py
+++ auth_service/app/core/security.py
@@ -6,12 +6,22 @@ import bcrypt
 from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
 
+# Security
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
 # Settings 
 from typing import Dict, Any, Optional
 from config.settings import settings
 
+from schema.token import TokenData
+from schema.user import UserResponse
+from schema.object_id import PyObjectId
+from repository.user import UserRepository, get_user_repository
 
 
+# ------------------- USER AUTHENTICATION HELPERS -------------------------
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/api/v1/users/login")
 
 # ------------------- PASSWORD ENCRYPTION HANDLERS ------------------------
 
@@ -27,9 +37,6 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(candidate_bytes, hashed_bytes)
 
 # -------------------------------------------------------------------------
-
-
-
 
 
 # ---------------------- JWT TOKEN HELPERS --------------------------------
@@ -53,15 +60,47 @@ def create_jwt_token(data : Dict[str, Any], expires_delta : Optional[timedelta] 
         algorithm = settings.security.jwt_algorithm
     )
 
-def decode_jwt_token(token : str) -> Optional[Dict[str, Any]]:
+def decode_jwt_token(token : str) -> Optional[TokenData]:
     try:
-        return jwt.decode(
+        payload = jwt.decode(
             token,
             settings.security.jwt_secret_key,
             algorithms = [settings.security.jwt_algorithm]
         )
+
+        sub : str = payload.get("sub")
+        if sub is None: 
+            return None
+        
+        return TokenData(
+            sub = sub
+        )
     
     except JWTError as e:
         return None
+    
+async def get_current_user(
+        token : str = Depends(oauth2_scheme),
+        user_repository : UserRepository = Depends(get_user_repository)
+) -> UserResponse:
+    token_data = decode_jwt_token(token)
+    if token_data is None:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Could not validate bearer token",
+            headers = {
+                "WWW-Authenticate": "Bearer"
+            }
+        )
+    
+    user_id = PyObjectId(token_data.sub)
+    current_user = await user_repository.find(id = user_id)
+    if current_user is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User does not exist" 
+        )
+    return UserResponse(**current_user.model_dump()) 
 
+    
 # -------------------------------------------------------------------------
