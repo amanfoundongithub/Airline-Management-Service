ec192d243258c65091847d2436f72a70cc638d98
Aman Raj <billygates124356@gmail.com>
2025-12-17 17:07:26 +0530

    Added lookup method for finding flight

--- flight_service/src/controller/flight.controller.ts
+++ flight_service/src/controller/flight.controller.ts
@@ -41,4 +41,34 @@ export class FlightController {
             FLIGHT_CONTROLLER_LOGGER.info("Completed request for creating new airline")
         }
     }
+
+    lookup = async (req : Request, res : Response) => {
+        const { flightNumber } = req.query;
+        try {
+
+            FLIGHT_CONTROLLER_LOGGER.info(`Request for Info on Airline #${flightNumber} received.`);
+            const flightDetails = await this.flightRepository.findByNumber(String(flightNumber).toUpperCase());
+
+            if(!flightDetails) {
+                FLIGHT_CONTROLLER_LOGGER.info(`Airline #${flightNumber} not found.`)
+                return res.status(404).json({
+                    message: "The request airline could not be found."
+                })
+            } else {
+                FLIGHT_CONTROLLER_LOGGER.info(`Airline #${flightNumber} found.`)
+                return res.status(200).json({
+                    message : "FOUND",
+                    details : flightDetails
+                })
+            }
+
+        } catch(e) {
+            FLIGHT_CONTROLLER_LOGGER.error(`Error in findind details: ${e}`);
+            return res.status(500).json({
+                message : e 
+            })
+        } finally {
+            FLIGHT_CONTROLLER_LOGGER.info(`Request for Info on Airline #${flightNumber} completed.`)
+        }
+    }
 }
