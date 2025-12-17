a6fa4d9883da14fe00fdcc264052b80e1dff7cac
Aman Raj <billygates124356@gmail.com>
2025-12-17 15:58:37 +0530

    Updated controller, removed middleware

--- flight_service/src/controller/flight.controller.ts
+++ flight_service/src/controller/flight.controller.ts
@@ -48,25 +48,3 @@ export class FlightController {
         }
     }
 }
-
-export const validateFlightCreationRequestMiddleware = (req : Request, res : Response, next : NextFunction) => {
-
-    const missingFields = findMissingFieldsInCreateRequest(req) 
-
-    if (missingFields.length > 0) {
-        return res.status(400).json({
-            error: "Some fields are missing to create the required aircraft.",
-            missingFields
-        });
-    }
-
-    if(!validateArrivalBeforeDeparture(req.body.arrival_time, req.body.departure_time)) {
-        return res.status(400).json({
-            error : "Arrival time must be before the departure time."
-        });
-    }
-
-    next() 
-    
-}
-
