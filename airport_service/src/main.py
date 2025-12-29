65c155e8b0f28cf2e830922bad9e31c51564dee0
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:06:46 +0530

    Updated main.py to include router v1

--- airport_service/src/main.py
+++ airport_service/src/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi           import FastAPI
+from src.api.v1.router import router as v1_router
 
 
 # Initialize the FastAPI application
 app = FastAPI()
+
+# Add the router to the main application
+app.include_router(v1_router)
