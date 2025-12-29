4332fe0d012c9a62992f7d17573efe89abb7863f
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:18:00 +0530

    Populated settings with .env values in __init__.py

---
+++ airport_service/src/core/settings/__init__.py
@@ -0,0 +1,10 @@
+from src.core.settings.service import ServiceSettings
+from src.core.settings.runtime import RuntimeSettings
+from src.core.settings.database import DatabaseSettings
+from src.core.settings.external import OpenFlightsSettings
+
+# Instantiate all the settings
+service_settings = ServiceSettings()
+runtime_settings = RuntimeSettings()
+openflights_settings = OpenFlightsSettings()
+database_settings = DatabaseSettings()
