4fd49928b2fa2bcd98aa1f4deab9eeaf2239c042
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:36:20 +0530

    Updated to routers

--- airport_service/src/api/v1/router.py
+++ airport_service/src/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.api.v1.routes.health_router import router as health_router
+from src.api.v1.routers.health_router import router as health_router
 
 # Main router
 router = APIRouter(prefix = "/api/v1")
