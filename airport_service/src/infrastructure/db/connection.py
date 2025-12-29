a6901290f5807736e232e1c2de2e01b060e6df64
Aman Raj <billygates124356@gmail.com>
2025-12-30 00:04:43 +0530

    Updated connection

--- airport_service/src/infrastructure/db/connection.py
+++ airport_service/src/infrastructure/db/connection.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
 from src.core.settings import database_settings
 import sqlite3
 
 def get_connection() -> sqlite3.Connection:
+    # Make the path if it ain't there
+    db_path = Path(database_settings.SQLITE_DB_PATH)
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+
     conn = sqlite3.connect(database_settings.SQLITE_DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
