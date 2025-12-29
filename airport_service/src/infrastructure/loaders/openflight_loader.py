8b2c6fa20b08d9e97524918949bdb25161ae080b
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:44:10 +0530

    Updated loader to src

--- airport_service/src/infrastructure/loaders/openflight_loader.py
+++ airport_service/src/infrastructure/loaders/openflight_loader.py
@@ -1,17 +1,18 @@
-from core.settings      import openflights_settings
-from helpers.exceptions import OpenFlightDownloadError, OpenFlightParseError
-from pathlib            import Path
+from src.core.settings   import openflights_settings
+from src.core.exceptions import OpenFlightParseError, OpenFlightDownloadError
+
+from pathlib           import Path
+from typing            import List
 
 import requests
 import csv
 
 
 class OpenFlightLoader:
-
     def __init__(self):
         self.url = openflights_settings.OPENFLIGHTS_URL
         self.path = Path(openflights_settings.OPENFLIGHTS_PATH)
-    
+
     def ensure_file(self) -> Path:
         if self.path.exists():
             return self.path
@@ -21,12 +22,12 @@ class OpenFlightLoader:
             response = requests.get(self.url, timeout = 30)
             response.raise_for_status()
         except Exception as e:
-            raise OpenFlightDownloadError(e) 
-    
+            raise OpenFlightDownloadError(e)
+
         self.path.write_bytes(response.content)
         return self.path
 
-    def load_rows(self) -> list[list[str]]:
+    def load_rows(self) -> List[List[str]]:
         path = self.ensure_file()
 
         try:
@@ -34,5 +35,4 @@ class OpenFlightLoader:
                 reader = csv.reader(f)
                 return list(reader)
         except Exception as e:
-            raise OpenFlightParseError(e) 
-    
+            raise OpenFlightParseError(e)
