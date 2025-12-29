e940b29f15cea5d8c8e576296ad696fc0372de9a
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:06:34 +0530

    Added health router

---
+++ airport_service/src/api/v1/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from src.api.v1.routes.health_router import router as health_router
+
+# Main router
+router = APIRouter(prefix = "/api/v1")
+router.include_router(health_router)
---
+++ airport_service/src/api/v1/routes/health_router.py
@@ -0,0 +1,13 @@
+from fastapi  import APIRouter
+from datetime import datetime
+
+# Router
+router = APIRouter()
+
+# Basic ping
+@router.get("/")
+async def ping():
+    return {
+        "timestamp" : str(datetime.now()),
+        "status" : "OK"
+    }
