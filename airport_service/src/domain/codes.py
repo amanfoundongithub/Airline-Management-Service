ba74d542a71987512a7bce28e7fc8556c038a2b1
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:23:33 +0530

    Updated with IATA and ICAO codes

--- airport_service/src/domain/codes.py
+++ airport_service/src/domain/codes.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 @dataclass(frozen = True)
 class IATACode:
-    value : str 
+    value : str
 
     def __post_init__(self):
         if len(self.value) != 3 or not self.value.isalpha():
-            raise ValueError(f"The proposed IATA code {self.value} is illegal.")
+            raise ValueError(f"The IATA Code {self.value} is not valid.")
         object.__setattr__(self, "value", self.value.upper())
 
 @dataclass(frozen = True)
@@ -15,5 +15,5 @@ class ICAOCode:
 
     def __post_init__(self):
         if len(self.value) != 4 or not self.value.isalpha():
-            raise ValueError(f"The proposed ICAO code {self.value} is illegal.")
+            raise ValueError(f"The ICAO Code {self.value} is not valid.")
         object.__setattr__(self, "value", self.value.upper())
