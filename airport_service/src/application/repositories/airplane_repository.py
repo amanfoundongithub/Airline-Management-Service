b1de4a1f6c2f15f6bf143dd112811729aa3dfb35
Aman Raj <billygates124356@gmail.com>
2025-12-29 23:33:52 +0530

    Added airplane repository

---
+++ airport_service/src/application/repositories/airplane_repository.py
@@ -0,0 +1,15 @@
+from abc    import ABC, abstractmethod
+from typing import List
+
+from src.domain.airport import Airport
+
+
+class AirplaneRepository(ABC):
+
+    @abstractmethod
+    def save(self, airports : List[Airport]) -> None:
+        pass
+
+    @abstractmethod
+    def get_by_iata(self, iata : str) -> Airport:
+        pass
