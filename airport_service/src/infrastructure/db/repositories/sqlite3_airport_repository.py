308400ca932048fa1ae6c35523661741b9f6b7b2
Aman Raj <billygates124356@gmail.com>
2025-12-30 18:02:23 +0530

    added implementation of new helpers

--- airport_service/src/infrastructure/db/repositories/sqlite3_airport_repository.py
+++ airport_service/src/infrastructure/db/repositories/sqlite3_airport_repository.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from src.application.repositories.airport_repository import AirportRepository
 from src.domain.airport                              import Airport
 from src.domain.codes                                import IATACode, ICAOCode
-from src.infrastructure.db.connection import get_connection
+from src.infrastructure.db.connection                import get_connection
 
 
 class SQLite3AirportRepository(AirportRepository):
@@ -25,14 +27,56 @@ class SQLite3AirportRepository(AirportRepository):
                                ) for a in airports
                            ])
 
-    def get_by_iata(self, iata : str) -> Airport:
+    def find_by_code(self, code : str) -> Airport:
         with get_connection() as cursor:
             row = cursor.execute("""
                                  SELECT *
                                  FROM airports
-                                 WHERE iata = ?
-                                 """, (iata.upper(),)).fetchone()
+                                 WHERE iata = ? OR icao = ?
+                                 """, (code.upper(), code.upper())).fetchone()
             return self._row_to_airport(row)
+
+    def find_by_params(self,
+                       city : str,
+                       country : str,
+                       limit : int = 50,
+                       offset : int = 0) -> List[Airport]:
+        with get_connection() as cursor:
+            query = """
+            SELECT * FROM airports WHERE 1=1"""
+            params = []
+            if city:
+                query += f""" AND city = ? COLLATE NOCASE """
+                params.append(city)
+            if country:
+                query += f""" AND country = ? COLLATE NOCASE """
+                params.append(country)
+
+            query += f"""LIMIT ? OFFSET ?"""
+            params.extend([limit, offset])
+
+            rows = cursor.execute(query, params).fetchall()
+            return [self._row_to_airport(row) for row in rows]
+
+    def find_by_query(self,
+                      q : str,
+                      limit : int = 50,
+                      offset : int = 0) -> List[Airport]:
+        with get_connection() as cursor:
+            query = """
+                SELECT *
+                FROM airports
+                WHERE
+                    LOWER(name) LIKE LOWER(?)
+                OR  LOWER(city) LIKE LOWER(?)
+                OR  iata LIKE ?
+                OR  icao LIKE ?
+                LIMIT ? OFFSET ?"""
+            search = f"%{q}%"
+            params = [search, search, search, search, limit, offset]
+            rows = cursor.execute(query, params).fetchall()
+            return [self._row_to_airport(row) for row in rows]
+
 
     def _row_to_airport(self, row) -> Airport:
         if not row:
