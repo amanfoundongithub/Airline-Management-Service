0b3875c7dd8618eb5ab627a531e947b1f64c1251
Aman Raj <billygates124356@gmail.com>
2025-12-30 23:34:32 +0530

    added logger, removed extra prints

--- airport_service/src/__init__.py
+++ airport_service/src/__init__.py
@@ -4,17 +4,11 @@ from src.infrastructure.db.migrate                import run_migrations
 from src.api.dependencies                         import get_airport_repository
 from src.core.logging.config                      import setup_logging
 
-print("[INFO] Starting the application...\n")
-
 # Configure logger before initialization of application
-print("[INFO] Setting up logging...")
 setup_logging()
-print("[INFO] Logger set up COMPLETED!\n")
 
 # Instantiate DB by running migrations
-print("[INFO] Setting up database...")
 run_migrations()
-print("[INFO] database set up COMPLETED!\n")
 
 # Now we will load the data of airport
 loader = OpenFlightLoader()
