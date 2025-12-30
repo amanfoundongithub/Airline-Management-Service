e49e4c8dea6457c58b4239c660da210d626ce742
Aman Raj <billygates124356@gmail.com>
2025-12-30 18:02:10 +0530

    Updated interface with more helpers

--- airport_service/src/application/repositories/airport_repository.py
+++ airport_service/src/application/repositories/airport_repository.py
@@ -1,15 +1,31 @@
-from abc    import ABC, abstractmethod
-from typing import List
-
+from abc                import ABC, abstractmethod
+from typing             import List
 from src.domain.airport import Airport
 
 
 class AirportRepository(ABC):
 
     @abstractmethod
-    def save(self, airports : List[Airport]) -> None:
+    def save(self,
+             airports : List[Airport]) -> None:
         pass
 
     @abstractmethod
-    def get_by_iata(self, iata : str) -> Airport:
+    def find_by_code(self,
+                     code : str) -> Airport:
+        pass
+
+    @abstractmethod
+    def find_by_params(self,
+                       city : str,
+                       country : str,
+                       limit : int = 50,
+                       offset : int = 0) -> List[Airport]:
+        pass
+
+    @abstractmethod
+    def find_by_query(self,
+                      q : str,
+                      limit : int = 50,
+                      offset : int = 0) -> List[Airport]:
         pass
