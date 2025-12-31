a6c90020e45d256bd074377b9da2f029bd655b83
Aman Raj <billygates124356@gmail.com>
2025-12-31 12:09:47 +0530

    added bootstrap to run the code

--- airport_service/src/main.py
+++ airport_service/src/main.py
@@ -5,12 +5,14 @@ from src.api.v1.router         import router as v1_router
 from src.api.health_router     import router as health_router
 from src.core.exceptions.http  import GenericHTTPException
 from src.core.settings         import service_settings
+from src.bootstrap             import bootstrap
 
 # Initialize the FastAPI application
 app = FastAPI(title = service_settings.SERVICE_NAME,
               description = service_settings.SERVICE_DESC,
               version = service_settings.SERVICE_VERSION)
 
+app.add_event_handler("startup", bootstrap)
 # Add exception handler for generic use
 app.add_exception_handler(GenericHTTPException, generic_exception_handler)
 
